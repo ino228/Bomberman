161006dea6d0bae5966a6066896b356bca7227e4
ino228 <30830033+ino228@users.noreply.github.com>
2020-01-23 21:20:52 +0100

    Graphic update
    background graphic added
    character graphic added

--- wall.py
+++ wall.py
@@ -4,5 +4,11 @@ class Wall:
     x = 0
     y = 0
     breakable = False
+    wallImg = None
     def __init__(self, x, y, breakable):
-        pass
+        self.x = x
+        self.y = y
+        self.breakable = breakable
+        self.wallImg = pygame.image.load("img/mur.png")
+    def draw(self, window):
+        window.blit(self.wallImg, (self.x, self.y))
